2907e42edc21a5a44b8a9d31ba38cdcb3598a354
hanny26 <hannyvyas109@gmail.com>
2024-06-09 23:45:18 +0530

    gitignore and other changes , like server js so implementingn to hide mongoose connectivity

--- server/index.js
+++ server/index.js
@@ -2,14 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const env = require('dotenv').config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
-
-const mongoUri = 'mongodb+srv://hannyvyas:hanny9054@cluster0.f9odr8p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const mongoUri = process.env.MONGO_URI;
+// const mongoUri = 'mongodb+srv://hannyvyas:hanny9054@cluster0.f9odr8p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
